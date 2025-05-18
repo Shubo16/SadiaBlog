81a6baaeac177cd7642807d2e6f8227d4451743a
Shubo <shubo2002@gmail.com>
2025-05-18 19:10:55 +0100

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     proxy: {
@@ -12,4 +13,5 @@ export default defineConfig({
       },
     },
   },
+  base: process.env.VITE_BASE_PATH || "/SadiaBlog"
 })
