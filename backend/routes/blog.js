a362acc98232095a4c1fb865aa0513e92b0a8a31
Shubo <shubo2002@gmail.com>
2025-05-24 12:44:46 +0100

    adding db file change

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../config/db";
+import pool from "../config/db.js";
 import dotenv from "dotenv";
 import multer from "multer";
 import path from "path";
