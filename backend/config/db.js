aa73d2bdf7cd77100ee0a9e402e7b8bf2136e4e9
Shubo <shubo2002@gmail.com>
2025-05-24 13:50:32 +0100

    databse production

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,17 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: isProduction
+    ? { rejectUnauthorized: false } // Required for Render
+    : false,                        // Disable SSL locally
 });
 
 pool.query("SELECT NOW()", (err, res) => {
