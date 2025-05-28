e4311ac0c6971d6f2c0f89a8c0f08954614c0aec
Shubo <shubo2002@gmail.com>
2025-05-28 14:56:10 +0100

    blog images fixed

--- frontend/src/services/backendApi.js
+++ frontend/src/services/backendApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "c";
+export const BASE_URL = "https://sadiablog.onrender.com";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_URL,
