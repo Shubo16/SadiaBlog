dd011b595ea8bff7df4a03834a58e169e4587d6c
Shubo <shubo2002@gmail.com>
2025-05-30 16:08:37 +0100

    change in proxy again

--- frontend/src/services/backendApi.js
+++ frontend/src/services/backendApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_URL = "https://sadiablog.onrender.com";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials:true
   // ...other configs
 });
