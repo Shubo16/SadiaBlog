e1b7fa4ab08dd336a49e2400d3968421f1ffb235
Shubo <shubo2002@gmail.com>
2025-05-30 13:14:31 +0100

    adjusting react icons in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons']
+    }
+  },
   server: {
     proxy: {
       '/api': {
