8c786fe74a9cd74c2ea4eb5a2ca07200f372bbc7
Shubo <shubo2002@gmail.com>
2025-05-30 16:17:00 +0100

    typo in index.js

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ app.use(passport.session());
 // Then CORS, JSON parsing, static files, etc.
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://sadiablog.onrender.com"],
+    origin: [process.env.BASE_URL_FRONTEND, process.env.BASE_URL],
     credentials: true,
   })
 );
