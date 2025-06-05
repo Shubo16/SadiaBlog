bf91601eddad67c73ff9e73f11527b02ed01f880
Shubo <shubo2002@gmail.com>
2025-06-05 18:11:12 +0100

    frontend changes

--- frontend/src/services/backendApi.js
+++ frontend/src/services/backendApi.js
@@ -5,7 +5,7 @@ export const BASE_URL = import.meta.env.VITE_BACKEND_PRODUCTION_URL;
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials:true,
-  Credential:include
+  credentials:include
   // ...other configs
 });
 
