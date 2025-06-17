20af99cea10f19e82401b59215d16bc1a9047cfd
Shubo <shubo2002@gmail.com>
2025-06-17 19:11:45 +0100

    speed vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // Updated to the correct path
 import { UserProvider } from './components/contexts/UserContext.jsx'
+import { SpeedInsightsProps } from '@vercel/speed-insights/*'
 
 
 createRoot(document.getElementById('root')).render(
